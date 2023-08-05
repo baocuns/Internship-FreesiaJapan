9b4131ef148ef4c0a7263737e5cf3eb1116885d7
Bao <baonv.23.student@fit.tdc.edu.vn>
2023-08-05 11:52:52 +0900

    Init source code Systems

---
+++ src/test/java/com/baocuns/systems/SystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baocuns.systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
